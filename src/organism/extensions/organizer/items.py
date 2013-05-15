kynikos/outspline

commit 12dbb691d4d23a9d6a703b1242fad2788988b7e9

use JSON for storing item rules in the database

src/organism/extensions/organizer/items.py
@@ -16,6 +16,8 @@
 # You should have received a copy of the GNU General Public License
 # along with Organism.  If not, see <http://www.gnu.org/licenses/>.
 
+import json
+
 from organism.coreaux_api import Event
 import organism.core_api as core_api
 
@@ -98,7 +100,7 @@ def insert_item(filename, id_, group, description='Insert item'):
 
     qconn = core_api.get_connection(filename)
     cursor = qconn.cursor()
-    cursor.execute(query_redo, ('', ))
+    cursor.execute(query_redo, (rules_to_string([]), ))
     core_api.give_connection(filename, qconn)
 
     core_api.insert_history(filename, group, id_, 'rules_insert', description,
@@ -108,30 +110,25 @@ def insert_item(filename, id_, group, description='Insert item'):
 def update_item_rules(filename, id_, rules, group,
                       description='Update item rules'):
     if isinstance(rules, list):
-        qrules = rules_to_string(rules)
-    else:
-        qrules = rules
-        rules = string_to_rules(rules)
+        rules = rules_to_string(rules)
 
     qconn = core_api.get_connection(filename)
     cursor = qconn.cursor()
     cursor.execute(queries.rules_select_id, (id_, ))
     sel = cursor.fetchone()
 
-    if sel:
-        unqrules = sel['R_rules']
-    else:
-        unqrules = ''
+    # The query should always return a result, so sel should never be None
+    unrules = sel['R_rules']
 
     query_redo = queries.rules_update_id.format(str(id_))
     query_undo = queries.rules_update_id.format(str(id_))
 
-    cursor.execute(query_redo, (qrules, ))
+    cursor.execute(query_redo, (rules, ))
 
     core_api.give_connection(filename, qconn)
 
     core_api.insert_history(filename, group, id_, 'rules_update', description,
-                            query_redo, qrules, query_undo, unqrules)
+                            query_redo, rules, query_undo, unrules)
 
     update_item_rules_event.signal(filename=filename, id_=id_)
 
@@ -168,10 +165,8 @@ def delete_item_rules(filename, id_, group, description='Delete item rules'):
     cursor.execute(queries.rules_select_id, (id_, ))
     sel = cursor.fetchone()
 
-    if sel:
-        current_rules = sel['R_rules']
-    else:
-        current_rules = ''
+    # The query should always return a result, so sel should never be None
+    current_rules = sel['R_rules']
 
     query_redo = queries.rules_delete_id.format(id_)
     query_undo = queries.rules_insert.format(id_)
@@ -192,43 +187,19 @@ def get_item_rules(filename, id_):
     row = cursor.fetchone()
     core_api.give_connection(filename, qconn)
 
-    if row:
-        return string_to_rules(row['R_rules'])
-    else:
-        return {}
+    # The query should always return a result, so row should never be None
+    return string_to_rules(row['R_rules'])
 
 
 def rules_to_string(rules):
-    string = ''
-    # It's possible that rules arrives here as None
-    if rules:
-        for rule in rules:
-            for key in rule:
-                sub = ':'.join((key, str(rule[key])))
-                string = ''.join((string, sub, ';'))
-            string = ''.join((string, '|'))
-
-    return string
+    # rules should always be a list, never equal to None
+    return json.dumps(rules, separators=(',',':'))
 
 
 def string_to_rules(string):
-    # Initialize rules in case string is False
-    rules = []
-    if string:
-        rules = string.split('|')
-        for i, v in enumerate(rules):
-            strule = v.split(';')
-            rules[i] = {}
-            for sub in strule:
-                spl = sub.split(':')
-                # Get rid of the empty string coming from the final ; created
-                # by rules_to_string()
-                if spl[0]:
-                    rules[i][spl[0]] = spl[1]
-
-    # The last element is empty due to how the string is formatted by
-    # rules_to_string()
-    return rules[:-1]
+    # Items without rules should have an empty list anyway (thus manageable by
+    # json.loads)
+    return json.loads(string)
 
 
 def get_occurrences(mint, maxt):

src/organism/extensions/organizer_basicrules/except_once.py
@@ -17,38 +17,23 @@
 # along with Organism.  If not, see <http://www.gnu.org/licenses/>.
 
 
-def normalize_rule(rule):
-    start = int(rule['start'])
-    end = int(rule['end'])
+def get_occurrences(mint, maxt, filename, id_, rule, tempoccs):
+    start = rule['start']
+    end = rule['end']
     inclusive = rule['inclusive']
-    
-    if inclusive == 'True':
-        inclusive = True
-    elif inclusive == 'False':
-        inclusive = False
-    
-    return (start, end, inclusive)
+
+    if start <= maxt and end >= mint:
+        tempoccs.except_(filename, id_, start, end, inclusive)
 
 
 def search_alarms(filename, id_, rule, alarms):
-    rule = normalize_rule(rule)
-    start = rule[0]
-    end = rule[1]
-    inclusive = rule[2]
-    
+    start = rule['start']
+    end = rule['end']
+    inclusive = rule['inclusive']
+
     limits = alarms.get_time_span()
     minstart = limits[0]
     maxend = limits[1]
 
     if start <= maxend and end >= minstart:
         alarms.except_(filename, id_, start, end, inclusive)
-
-
-def get_occurrences(mint, maxt, filename, id_, rule, tempoccs):
-    rule = normalize_rule(rule)
-    start = rule[0]
-    end = rule[1]
-    inclusive = rule[2]
-    
-    if start <= maxt and end >= mint:
-        tempoccs.except_(filename, id_, start, end, inclusive)

src/organism/extensions/organizer_basicrules/occur_every_day.py
@@ -19,27 +19,7 @@
 import time as _time
 
 
-def normalize_rule(reftime, rule):
-    rstart = int(rule['rstart'])
-    rendn = rule['rendn']
-    rendu = rule['rendu']
-    ralarm = rule['ralarm']
-    
-    if rendn == 'None':
-        rendn = None
-    # rendn could have been already None (not a string)
-    elif rendn != None:
-        rendn = int(rendn)
-    
-    if rendu == 'None':
-        rendu = None
-    
-    if ralarm == 'None':
-        ralarm = None
-    # alarm could have been already None (not a string)
-    elif ralarm != None:
-        ralarm = int(ralarm)
-            
+def _compute_rend(rendn, rendu):
     if rendn != None:
         mult = {'minutes': 60,
                 'hours': 3600,
@@ -47,91 +27,79 @@ def normalize_rule(reftime, rule):
                 'weeks': 604800,
                 'months': 2592000,
                 'years': 31536000}
-        
-        rend = rendn * mult[rendu]
+
+        return rendn * mult[rendu]
     else:
-        rend = None
-    
+        return None
+
+
+def _compute_start(reftime, rstart, rend):
     firstday = reftime // 86400 * 86400 + _time.altzone
     rday = reftime % 86400
-    
+
     if rstart < rday:
-        start = firstday + 86400 + rstart
+        startt = firstday + 86400 + rstart
     else:
-        start = firstday + rstart
-    
-    return (start, rend, ralarm)
+        startt = firstday + rstart
 
-
-def search_alarms(last_search, filename, id_, rule, alarms):
-    rule = normalize_rule(last_search, rule)
-    rend = rule[1]
-    ralarm = rule[2]
-    
     if rend != None:
-        # This algorithm must get also occurrences whose end time is the next
-        # alarm
-        start = rule[0] - 86400 * (1 + rend // 86400)
+        # This algorithm must also get occurrences whose end time falls within
+        # the requested range (for get_occurrences) or is the next alarm (for
+        # search_alarms)
+        return startt - 86400 * (1 + rend // 86400)
     else:
-        start = rule[0]
-    
-    while True:
-        if rend != None:
-            end = start + rend
-        else:
-            end = None
-        
-        if ralarm != None:
-            alarm = start - ralarm
-        else:
-            alarm = None
-        
-        alarmd = {'filename': filename,
-                  'id_': id_,
-                  'start': start,
-                  'end': end,
-                  'alarm': alarm}
-        
-        next_alarm = alarms.get_next_alarm()
-        
-        if alarms.add(last_search, alarmd) or (next_alarm and
-                                      (alarm is None and start > next_alarm) or
-                                               (alarm and alarm > next_alarm)):
-            break
-        
-        start += 86400
+        return startt
+
+
+def _compute_end(start, rend):
+    return start + rend if rend != None else None
+
+
+def _compute_alarm(start, ralarm):
+    return start - ralarm if ralarm != None else None
 
 
 def get_occurrences(mint, maxt, filename, id_, rule, tempoccs):
-    rule = normalize_rule(mint, rule)
-    rend = rule[1]
-    ralarm = rule[2]
-    
-    if rend != None:
-        # This algorithm must get also occurrences whose end time falls within
-        # the requested range
-        start = rule[0] - 86400 * (1 + rend // 86400)
-    else:
-        start = rule[0]
-    
+    rend = _compute_rend(rule['rendn'], rule['rendu'])
+    start = _compute_start(mint, rule['rstart'], rend)
+    ralarm = rule['ralarm']
+
     while True:
-        if rend != None:
-            end = start + rend
-        else:
-            end = None
-        
-        if ralarm != None:
-            alarm = start - ralarm
-        else:
-            alarm = None
-        
+        end = _compute_end(start, rend)
+        alarm = _compute_end(start, ralarm)
+
         if (alarm and alarm > maxt) or start > maxt:
             break
-        
+
         tempoccs.add({'filename': filename,
                       'id_': id_,
                       'start': start,
                       'end': end,
                       'alarm': alarm})
-        
+
+        start += 86400
+
+
+def search_alarms(last_search, filename, id_, rule, alarms):
+    rend = _compute_rend(rule['rendn'], rule['rendu'])
+    start = _compute_start(last_search, rule['rstart'], rend)
+    ralarm = rule['ralarm']
+
+    while True:
+        end = _compute_end(start, rend)
+        alarm = _compute_end(start, ralarm)
+
+        alarmd = {'filename': filename,
+                  'id_': id_,
+                  'start': start,
+                  'end': end,
+                  'alarm': alarm}
+
+        next_alarm = alarms.get_next_alarm()
+
+        if alarms.add(last_search, alarmd) or (next_alarm and
+                                       (alarm is None and start > next_alarm) or
+                                                (alarm and alarm > next_alarm)):
+            break
+
         start += 86400

src/organism/extensions/organizer_basicrules/occur_once.py
@@ -17,47 +17,32 @@
 # along with Organism.  If not, see <http://www.gnu.org/licenses/>.
 
 
-def normalize_rule(rule):
-    start = int(rule['start'])
-    end = rule['end']
-    ralarm = rule['ralarm']
-    
-    if end == 'None':
-        end = None
-    # end could have been already None (not a string)
-    elif end != None:
-        end = int(end)
-    
-    if ralarm == 'None':
-        alarm = None
-    elif ralarm == None:
-        alarm = None
-    elif ralarm != None:
-        alarm = start - int(ralarm)
-    
-    return (start, end, alarm)
-    
-def search_alarms(last_search, filename, id_, rule, alarms):
-    rule = normalize_rule(rule)
-    start = rule[0]
-    end = rule[1]
-    alarm = rule[2]
-    
-    alarms.add(last_search, {'filename': filename,
-                             'id_': id_,
-                             'start': start,
-                             'end': end,
-                             'alarm': alarm})
+def _compute_alarm(start, ralarm):
+    return None if (ralarm == None) else (start - ralarm)
 
 
 def get_occurrences(filename, id_, rule, tempoccs):
-    rule = normalize_rule(rule)
-    start = rule[0]
-    end = rule[1]
-    alarm = rule[2]
-    
+    start = rule['start']
+    end = rule['end']
+    ralarm = rule['ralarm']
+
+    alarm = _compute_alarm(start, ralarm)
+
     tempoccs.add({'filename': filename,
                   'id_': id_,
                   'start': start,
                   'end': end,
                   'alarm': alarm})
+
+def search_alarms(last_search, filename, id_, rule, alarms):
+    start = rule['start']
+    end = rule['end']
+    ralarm = rule['ralarm']
+
+    alarm = _compute_alarm(start, ralarm)
+
+    alarms.add(last_search, {'filename': filename,
+                             'id_': id_,
+                             'start': start,
+                             'end': end,
+                             'alarm': alarm})

src/organism/plugins/wxscheduler_basicrules/except_once.py
@@ -165,21 +165,11 @@ class Rule():
         end = kwargs['rule']['end']
         inclusive = kwargs['rule']['inclusive']
 
-        start = int(start)
-        end = int(end)
-
-        if inclusive == 'True':
-            inclusive = True
-        else:
-            inclusive = False
-
         cls.create_rule(filename=kwargs['filename'], id_=kwargs['id_'],
                         start=start, end=end, inclusive=inclusive)
 
     @staticmethod
     def create_rule(filename, id_, start, end, inclusive):
-        start = int(start)
-        end = int(end)
         label = ' '.join(('Except', _time.strftime('from %a %d %b %Y at %H:%M',
                            _time.localtime(start)),
                            _time.strftime('until %a %d %b %Y at %H:%M',

src/organism/plugins/wxscheduler_basicrules/occur_every_day.py
@@ -159,27 +159,11 @@ class Rule():
         rendu = kwargs['rule']['rendu']
         ralarm = kwargs['rule']['ralarm']
 
-        rstart = int(rstart)
-
-        if rendn == 'None':
-            rendn = None
-        else:
-            rendn = int(rendn)
-
-        if rendu == 'None':
-            rendu = None
-
-        if ralarm == 'None':
-            ralarm = None
-        else:
-            ralarm = int(ralarm)
-
         cls.create_rule(filename=kwargs['filename'], id_=kwargs['id_'],
                         rstart=rstart, rendn=rendn, rendu=rendu, ralarm=ralarm)
 
     @staticmethod
     def create_rule(filename, id_, rstart, rendn, rendu, ralarm):
-        rstart = int(rstart)
         label = 'Occur every day at {}:{}'.format(str(rstart // 3600).zfill(2),
                                               str(rstart % 3600 // 60).zfill(2))
 

src/organism/plugins/wxscheduler_basicrules/occur_once.py
@@ -190,30 +190,16 @@ class Rule():
         end = kwargs['rule']['end']
         ralarm = kwargs['rule']['ralarm']
 
-        start = int(start)
-
-        if end == 'None':
-            end = None
-        else:
-            end = int(end)
-
-        if ralarm == 'None':
-            ralarm = None
-        else:
-            ralarm = int(ralarm)
-
         cls.create_rule(filename=kwargs['filename'], id_=kwargs['id_'],
                         start=start, end=end, ralarm=ralarm)
 
     @staticmethod
     def create_rule(filename, id_, start, end, ralarm):
-        start = int(start)
         label = ' '.join(('Occur once',
                            _time.strftime('from %a %d %b %Y at %H:%M',
                            _time.localtime(start))))
 
         if end != None:
-            end = int(end)
             label += _time.strftime(' until %a %d %b %Y at %H:%M',
                                     _time.localtime(end))
 

commit 9215ea1e64e360b8cbe254f5f57f659a0823d2ec

fix bug introduced with 12dbb691d4d23a9d6a703b1242fad2788988b7e9

src/organism/extensions/organizer/items.py
@@ -104,7 +104,7 @@ def insert_item(filename, id_, group, description='Insert item'):
     core_api.give_connection(filename, qconn)
 
     core_api.insert_history(filename, group, id_, 'rules_insert', description,
-                            query_redo, '', query_undo, '')
+                            query_redo, rules_to_string([]), query_undo, None)
 
 
 def update_item_rules(filename, id_, rules, group,
@@ -176,7 +176,7 @@ def delete_item_rules(filename, id_, group, description='Delete item rules'):
     core_api.give_connection(filename, qconn)
 
     return core_api.insert_history(filename, group, id_, 'rules_delete',
-                                   description, query_redo, '', query_undo,
+                                   description, query_redo, None, query_undo,
                                    current_rules)
 
 