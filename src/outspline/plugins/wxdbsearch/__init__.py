fb48a3c2d42be59973a02221d8421c41ef300902
Dario Giovannetti <dev@dariogiovannetti.net>
2013-12-29 18:45:43 +0800

    Retrieve results

--- src/outspline/plugins/wxdbsearch/__init__.py
+++ src/outspline/plugins/wxdbsearch/__init__.py
@@ -16,11 +16,14 @@
 # You should have received a copy of the GNU General Public License
 # along with Outspline.  If not, see <http://www.gnu.org/licenses/>.
 
+import re
 import wx
 
 import outspline.coreaux_api as coreaux_api
 import outspline.core_api as core_api
 import outspline.interfaces.wxgui_api as wxgui_api
+
+import exceptions
 
 
 class SearchView():
@@ -34,32 +37,67 @@ class SearchView():
         self.box = wx.BoxSizer(wx.VERTICAL)
         self.panel.SetSizer(self.box)
 
-        self.filters = SearchFilters(self.panel)
-        self.results = SearchResults(self.panel)
+        self.filters = SearchFilters(self)
+        self.results = SearchResults(self)
 
         self.box.Add(self.filters.box, flag=wx.EXPAND)
         self.box.Add(self.results.list_, 1, flag=wx.EXPAND)
 
+    def search(self, event):
+        # Search must be threaded ***********************************************************
+        core_api.block_databases()
+
+        string = self.filters.text.GetValue()
+        string = re.escape(string)  # Escape only if needed **********************************
+
+        try:
+            regexp = re.compile(string, re.IGNORECASE)  # Add flags as needed **************
+        except re.error:
+            pass  # Show error dialog *******************************************************
+        else:
+            for filename in core_api.get_open_databases():
+                rows = core_api.get_all_items_text(filename)
+
+                for row in rows:
+                    id_ = row['I_id']
+                    text = row['I_text']
+
+                    matches = []
+
+                    for match in regexp.finditer(text):
+                        matches.append(match)
+                        # break if one result per item **********************************
+
+                    # It's necessary to aggregate the matches first because
+                    # only one match per line should be shown
+                    if matches:
+                        self.results.add(filename, id_, text, matches)
+
+        core_api.release_databases()
+
 
 class SearchFilters():
-    # Search in all databases / in selected database / under selected items  ************
-    # Regular expression ****************************************************************
-    # Case sensitive ********************************************************************
-    # List of words (OR) ****************************************************************
-    # Whole words (not part of words) ***************************************************
-    # Invert results ********************************************************************
+    # Search in all databases / in selected database / under selected items  **********
+    # Regular expression **************************************************************
+    # Case sensitive ******************************************************************
+    # List of words (OR) **************************************************************
+    # Whole words (not part of words) *************************************************
+    # Invert results ******************************************************************
+    # Show only one result per item ***************************************************
     box = None
     text = None
     search = None
 
-    def __init__(self, parent):
+    def __init__(self, mainview):
         self.box = wx.BoxSizer(wx.HORIZONTAL)
 
-        self.text = wx.TextCtrl(parent)
+        self.text = wx.TextCtrl(mainview.panel)
         self.box.Add(self.text, 1, flag=wx.ALIGN_CENTER_VERTICAL)
 
-        self.search = wx.Button(parent)
+        self.search = wx.Button(mainview.panel, label='Search')
         self.box.Add(self.search, flag=wx.ALIGN_CENTER_VERTICAL)
+
+        mainview.panel.Bind(wx.EVT_BUTTON, mainview.search, self.search)
 
 
 class SearchResults():
@@ -69,8 +107,73 @@ class SearchResults():
     #   Matching line    **************************************************************
     list_ = None
 
-    def __init__(self, parent):
-        self.list_ = wx.ListView(parent)
+    def __init__(self, mainview):
+        self.list_ = wx.ListView(mainview.panel)
+
+    def add(self, filename, id_, text, matches):
+        heading = self.get_heading(filename, id_)
+
+        # I have to use an iterator because previous_line_index must be
+        # initialized at the first iteration
+        iterator = iter(matches)
+
+        match = iterator.next()
+        previous_line_index, line = self.find_first_match_line(text,
+                                                                match.start())
+        self.show(filename, heading, line)
+
+        while True:
+            try:
+                match = iterator.next()
+            except StopIteration:
+                break
+            else:
+                try:
+                    previous_line_index, line = self.find_match_line(text,
+                                            previous_line_index, match.start())
+                except exceptions.MatchIsInSameLineError:
+                    pass
+                else:
+                    self.show(filename, heading, line)
+
+    def find_first_match_line(self, text, match_start):
+        try:
+            # Add 1 so that the line doesn't start with the '\n'
+            line_start = text.rindex('\n', 0, match_start) + 1
+        except ValueError:
+            # The match is in the first line
+            line_start = 0
+
+        return self._find_match_line_end(text, line_start)
+
+    def find_match_line(self, text, previous_line_index, match_start):
+        try:
+            # Add 1 so that the line doesn't start with the '\n'
+            line_start = text.rindex('\n', previous_line_index, match_start) \
+                                                                            + 1
+        except ValueError:
+            # The match is in the line of the previous match
+            raise exceptions.MatchIsInSameLineError()
+        else:
+            return self._find_match_line_end(text, line_start)
+
+    def _find_match_line_end(self, text, line_start):
+        try:
+            line_end = text.index('\n', line_start)
+        except ValueError:
+            # The match is in the last line
+            line_end = len(text)
+
+        line = text[line_start:line_end]
+
+        return (line_start, line)
+
+    def get_heading(self, filename, id_):
+        text = core_api.get_item_text(filename, id_)
+        return text.partition('\n')[0]
+
+    def show(self, filename, heading, line):
+        print('ZZZ', filename, heading, line)  # *******************************************************************
 
 
 class MainMenu(wx.Menu):
