3f8ab8bfc8c72a8e77df5a292785e7061b6771c9
Dario Giovannetti <dev@dariogiovannetti.net>
2014-04-05 23:11:36 +0800

    Deprecate provides_history_actions parameter in .info files
    Fixes #164

--- src/outspline/coreaux/addons.py
+++ src/outspline/coreaux/addons.py
@@ -25,19 +25,17 @@ import exceptions
 from logger import log
 
 
-def load_addon(faddon, reqversion, tablenames, historynames):
+def load_addon(faddon, reqversion, tablenames):
     '''
     Poss. cases | BASE               | DEPENDENCY         | OPTIONAL
-    ------------+--------------------+--------------------+---------------------
+    ------------+--------------------+--------------------+--------------------
     NOT FOUND   | impossible         | critical exception | debug message
-    ------------+--------------------+--------------------+---------------------
+    ------------+--------------------+--------------------+--------------------
     DISABLED    | debug message      | critical exception | debug message
-    ------------+--------------------+--------------------+---------------------
+    ------------+--------------------+--------------------+--------------------
     VERSION     | impossible         | critical exception | critical exception
-    ------------+--------------------+--------------------+---------------------
+    ------------+--------------------+--------------------+--------------------
     TABLES      | critical exception | critical exception | critical exception
-    ------------+--------------------+--------------------+---------------------
-    HISTORY     | critical exception | critical exception | critical exception
     '''
     try:
         folder, addon = faddon.split('.')
@@ -59,14 +57,13 @@ def load_addon(faddon, reqversion, tablenames, historynames):
         mfaddon = '.'.join(('outspline', faddon))
 
         # An addon may list a dependency that is not installed
-        # This check must be done before the other ones, in fact if the addon is
-        # not installed it's impossible to read its info
+        # This check must be done before the other ones, in fact if the addon
+        # is not installed it's impossible to read its info
         if addon not in configuration.config(section).get_sections():
             raise exceptions.AddonNotFoundError()
 
-        # This check must be done before the version or the provided tables or
-        # history actions ones, in fact if an addon is disabled these problems
-        # shouldn't matter
+        # This check must be done before the version or the provided tables
+        # ones, in fact if an addon is disabled these problems shouldn't matter
         if not configuration.config(section)(addon).get_bool('enabled'):
             raise exceptions.AddonDisabledError()
 
@@ -87,27 +84,15 @@ def load_addon(faddon, reqversion, tablenames, historynames):
         # for the reason
         if mfaddon not in sys.modules:
             ptables = {t: faddon for t in info['provides_tables'].split(' ')
-                                                                           if t}
+                                                                        if t}
             test = [t for t in set(tablenames) & set(ptables)
-                                                 if tablenames[t] != ptables[t]]
+                                                if tablenames[t] != ptables[t]]
 
             if test:
                 raise exceptions.AddonProvidedTablesError(test,
-                                                  [tablenames[t] for t in test])
+                                                [tablenames[t] for t in test])
 
             tablenames.update(ptables)
-
-            phistory = {a: faddon for a in info['provides_history_actions'
-                                                                  ].split(' ')
-                                                                           if a}
-            test = [a for a in set(historynames) & set(phistory)
-                                              if historynames[a] != phistory[a]]
-
-            if test:
-                raise exceptions.AddonProvidedHistoryActionsError(test,
-                                                [historynames[a] for a in test])
-
-            historynames.update(phistory)
 
             deps = []
             opts = []
@@ -122,8 +107,7 @@ def load_addon(faddon, reqversion, tablenames, historynames):
 
             for d in deps:
                 try:
-                    load_addon(*d, tablenames=tablenames,
-                                                      historynames=historynames)
+                    load_addon(*d, tablenames=tablenames)
                 # If I wanted to silently disable an addon in case one of its
                 # dependencies is not satisfied (not found, disabled...) I
                 # should disable the addon in the configuration to prevent the
@@ -132,63 +116,57 @@ def load_addon(faddon, reqversion, tablenames, historynames):
                 # dependency which is also enabled and activated; this optional
                 # dependency, though, has a dependency which is not enabled, so
                 # it is not imported by this load_addon() function; however,
-                # since in the configuration it is enabled, it's imported by the
-                # main addon anyway with
-                # coreaux_api.import_optional_extension_api(), thus breaking the
-                # application, since the dependency for the optional dependency
-                # is still missing
+                # since in the configuration it is enabled, it's imported by
+                # the main addon anyway with
+                # coreaux_api.import_optional_extension_api(), thus breaking
+                # the application, since the dependency for the optional
+                # dependency is still missing
                 # Note that this change won't be written in the configuration
                 # file, since it's updated with config.export_add()
                 #except ...:
                 #    configuration.config(section)(addon)['enabled'] = 'off'
                 except exceptions.AddonNotFoundError:
-                    log.error(faddon + ' depends on ' + d[0] + ' which however '
-                                                              'cannot be found')
-                    # Raise a different exception, otherwise it may be caught by
-                    # start_addons()
+                    log.error(faddon + ' depends on ' + d[0] +
+                                            ' which however cannot be found')
+                    # Raise a different exception, otherwise it may be caught
+                    # by start_addons()
                     raise exceptions.AddonDependencyError()
                 except exceptions.AddonDisabledError:
-                    log.error(faddon + ' depends on ' + d[0] + ' which however '
-                                                                  'is disabled')
+                    log.error(faddon + ' depends on ' + d[0] +
+                                                ' which however is disabled')
                     # Raise a different exception, otherwise it will be caught
                     # by start_addons()
                     raise exceptions.AddonDependencyError()
                 except exceptions.AddonVersionError as err:
-                    log.error(faddon + ' depends on ' + d[0] + ' ' + str(d[1]) +
-                                   ' which however is installed with version ' +
-                                                               str(err.version))
-                    # Raise a different exception, otherwise it may be caught by
-                    # start_addons()
+                    log.error(faddon + ' depends on ' + d[0] + ' ' +
+                                str(d[1]) +
+                                ' which however is installed with version ' +
+                                str(err.version))
+                    # Raise a different exception, otherwise it may be caught
+                    # by start_addons()
                     raise exceptions.AddonDependencyError()
                 except exceptions.AddonProvidedTablesError as err:
                     log.error(faddon + ' depends on ' + d[0] + ' which '
-                                    'provides tables ' + ', '.join(err.tables) +
-                       ' that are already provided by ' + ', '.join(err.addons))
-                    # Raise a different exception, otherwise it will be caught
-                    # by start_addons()
-                    raise exceptions.AddonDependencyError()
-                except exceptions.AddonProvidedHistoryActionsError as err:
-                    log.error(faddon + ' depends on ' + d[0] + ' which '
-                          'provides history actions ' + ', '.join(err.actions) +
-                       ' that are already provided by ' + ', '.join(err.addons))
+                                'provides tables ' + ', '.join(err.tables) +
+                                ' that are already provided by ' +
+                                ', '.join(err.addons))
                     # Raise a different exception, otherwise it will be caught
                     # by start_addons()
                     raise exceptions.AddonDependencyError()
 
             for o in opts:
                 try:
-                    load_addon(*o, tablenames=tablenames,
-                                                      historynames=historynames)
+                    load_addon(*o, tablenames=tablenames)
                 except exceptions.AddonNotFoundError:
                     log.debug(faddon + ' optionally depends on ' + o[0] +
-                                               ' which however cannot be found')
+                                            ' which however cannot be found')
                 except exceptions.AddonDisabledError:
                     log.debug(faddon + ' optionally depends on ' + o[0] +
-                                                   ' which however is disabled')
+                                                ' which however is disabled')
                 except exceptions.AddonVersionError as err:
                     log.error(faddon + ' optionally depends on ' + o[0] +
-                                          ' ' + str(o[1]) + ' which however is '
-                                   'installed with version ' + str(err.version))
+                                ' ' + str(o[1]) + ' which however is '
+                                'installed with version ' + str(err.version))
                     # Just crash the application, in fact it's not easy to
                     # handle this case, as the same addon may be required by
                     # another addon with the correct version, but still this
@@ -198,20 +176,10 @@ def load_addon(faddon, reqversion, tablenames, historynames):
                     raise exceptions.AddonDependencyError()
                 except exceptions.AddonProvidedTablesError as err:
                     log.error(faddon + ' optionally depends on ' + o[0] +
-                             ' which provides tables ' + ', '.join(err.tables) +
-                       ' that are already provided by ' + ', '.join(err.addons))
-                    # Just crash the application, in fact it's not easy to
-                    # handle this case, as the same addon may be required by
-                    # another addon with the correct version, but still this
-                    # addon should *not* use this dependency
-                    # Raise a different exception, otherwise it will be caught
-                    # by start_addons()
-                    raise exceptions.AddonDependencyError()
-                except exceptions.AddonProvidedHistoryActionsError as err:
-                    log.error(faddon + ' optionally depends on ' + o[0] +
-                                            ' which provides history actions ' +
-                     ', '.join(err.actions) + ' that are already provided by ' +
-                                                          ', '.join(err.addons))
+                                            ' which provides tables ' +
+                                            ', '.join(err.tables) +
+                                            ' that are already provided by ' +
+                                            ', '.join(err.addons))
                     # Just crash the application, in fact it's not easy to
                     # handle this case, as the same addon may be required by
                     # another addon with the correct version, but still this
@@ -233,8 +201,6 @@ def start_addons():
     info = configuration.info('Core')
 
     tablenames = {t: 'core' for t in info['provides_tables'].split(' ') if t}
-    historynames = {a: 'core' for a in info['provides_history_actions'
-                                                              ].split(' ') if a}
 
     # Use a tuple because a simple dictionary doesn't keep sequence order
     for section, folder in (('Extensions', 'extensions'),
@@ -249,21 +215,17 @@ def start_addons():
             faddon = folder + '.' + pkg
 
             try:
-                load_addon(faddon, False, tablenames, historynames)
+                load_addon(faddon, False, tablenames)
             except exceptions.AddonDisabledError:
                 log.debug(faddon + ' is disabled')
             except exceptions.AddonProvidedTablesError as err:
                 log.error(faddon +  ' provides tables ' +
-                     ', '.join(err.tables) + ' which are already provided by ' +
-                                                          ', '.join(err.addons))
+                                            ', '.join(err.tables) +
+                                            ' which are already provided by ' +
+                                            ', '.join(err.addons))
                 raise
-            except exceptions.AddonProvidedHistoryActionsError as err:
-                log.error(faddon + ' provides history actions ' +
-                    ', '.join(err.actions) + ' which are already provided by ' +
-                                                          ', '.join(err.addons))
-                raise
-            # If wanting to catch other exceptions here that come only from base
-            # addons (not propagated from dependencies), remember to raise
+            # If wanting to catch other exceptions here that come only from
+            # base addons (not propagated from dependencies), remember to raise
             # different exceptions for any exception that is caught in
             # load_addon() (i.e. don't propagate the same exception, use e.g.
             # exceptions.AddonDependencyError)
