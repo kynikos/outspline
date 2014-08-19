82f55f5981e98d9f6b5173b5d2518be35c3c86ee
Dario Giovannetti <dev@dariogiovannetti.net>
2014-08-19 22:24:19 +0800

    Update setup files

--- src/outspline-experimental.setup.py
+++ src/outspline-experimental.setup.py
@@ -62,7 +62,7 @@ def compose_package_metadata(head):
 
 def compose_metadata():
     meta = {'name': 'outspline-experimental',
-            'version': '0.5.0',
+            'version': '0.6.0',
             'description': 'Experimental component for Outspline.',
             'author': 'Dario Giovannetti',
             'author_email': 'dev@dariogiovannetti.net',
--- src/outspline.setup.py
+++ src/outspline.setup.py
@@ -62,7 +62,7 @@ def compose_package_metadata(head):
 
 def compose_metadata():
     meta = {'name': 'outspline',
-            'version': '0.5.0',
+            'version': '0.6.0',
             'description': 'Highly modular and extensible outliner.',
             'author': 'Dario Giovannetti',
             'author_email': 'dev@dariogiovannetti.net',
