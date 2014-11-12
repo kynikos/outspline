1f2f190f98a23e2be88ec5e0695d1060de7d5d8d
Dario Giovannetti <dev@dariogiovannetti.net>
2014-11-12 19:00:52 +0800

    Split distribution-specific code from release.py

---
+++ dev/release-arch.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+# This script is supposed to be run in the ./dev directory as ./release-arch.py
+# If the -l option is passed, the external libraries won't be bundled and will
+#  have to be installed on the system separately
+# It's possible to build only some components by specifying them as arguments
+#  for the command
+
+import sys
+import os
+import shutil
+import subprocess
+
+import release
+
+
+def _make_pkgbuild(pkgname):
+    pkgbuild = os.path.join(release.DEST_DIR, pkgname + '.PKGBUILD')
+
+    subprocess.call(["updpkgsums", pkgbuild])
+
+    tmppkgbuild = os.path.join(release.DEST_DIR, 'PKGBUILD')
+    shutil.copy2(pkgbuild, tmppkgbuild)
+
+    subprocess.call(["mkaurball", ])
+    # Don't call makepkg --clean or errors will happen
+
+    os.remove(tmppkgbuild)
+
+
+if __name__ == '__main__':
+    release.main(sys.argv, _make_pkgbuild)
